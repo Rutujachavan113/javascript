4ef4ef2d2f0bb9ded19d6acb394aea4c664e8f31
Rutuja Chavan <131894779+Rutujachavan113@users.noreply.github.com>
2026-08-24 14:47:45 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,21 @@
+let value=3;
+let negValue=-value;
+console.log(negValue);
+
+let str1="hello"
+let str2=" world"
+console.log(str1+str2)
+console.log(1+2);
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+// null camparisons
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+
+// == and ===
+console.log("2"==2)//true
+console.log("2"===2)//false
