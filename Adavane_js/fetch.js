e3962a93e20b84c5fce4c62edbccbb7ac4576af5
Rutuja Chavan <131894779+Rutujachavan113@users.noreply.github.com>
2026-09-08 10:01:04 +0000

    fetch

---
+++ Adavane_js/fetch.js
@@ -0,0 +1,12 @@
+const users=fetch("https://api.github.com/users")
+.then((response)=>{
+    return response.json();
+})
+.then((data)=>{
+    console.log(data);
+    
+})
+.catch((error)=>{
+    console.log(error);
+    
+})
