12dae4331e9a6607b9a45d3193bf984323c31608
Rutuja Chavan <131894779+Rutujachavan113@users.noreply.github.com>
2026-09-12 08:22:49 +0000

    packed and Holey array

---
+++ Array_inDepth/Array_types.js
@@ -0,0 +1,13 @@
+//1. Continuous / Packed Array-->A packed array means the array has values at every index without gaps.
+
+const arr = [10, 20, 30, 40];
+
+console.log(arr);
+// 10 → 20 → 30 → 40
+
+// 2. Holey Array-->A holey array has an empty/missing index.
+const arr2= [10, , 30, 40];
+
+console.log(arr);
+// 10 → empty → 30 → 40
+
